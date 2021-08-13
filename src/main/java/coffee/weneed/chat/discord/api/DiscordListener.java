e2c93c2b3b1923b35c82bdbd7b5d6675b538a9ec
Daleth Darko <Daleth@Dalethium.com>
2021-08-13 09:35:05 +0200

    Emoji handling + build

--- src/main/java/coffee/weneed/chat/discord/api/DiscordListener.java
+++ src/main/java/coffee/weneed/chat/discord/api/DiscordListener.java
@@ -2,11 +2,17 @@ package coffee.weneed.chat.discord.api;
 
 import coffee.weneed.chat.discord.api.handlers.IJoinHandler;
 import coffee.weneed.chat.discord.api.handlers.IMessageHandler;
+import coffee.weneed.chat.discord.api.handlers.IReactionAddHandler;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class DiscordListener extends ListenerAdapter {
+	@Override
+	public void onMessageReactionAdd(MessageReactionAddEvent event) {
+		for (IReactionAddHandler handler : HandlerRegistry.getReactionAddHandlers()) if (handler.handles(event)) handler.handleEvent(event);
+	}
 	@Override
 	public void onGuildMemberJoin(GuildMemberJoinEvent event) {
 		for (IJoinHandler handler : HandlerRegistry.getJoinHandlers()) if (handler.handles(event)) handler.handleEvent(event);
--- src/main/java/coffee/weneed/chat/discord/api/HandlerRegistry.java
+++ src/main/java/coffee/weneed/chat/discord/api/HandlerRegistry.java
@@ -2,6 +2,7 @@ package coffee.weneed.chat.discord.api;
 
 import coffee.weneed.chat.discord.api.handlers.IJoinHandler;
 import coffee.weneed.chat.discord.api.handlers.IMessageHandler;
+import coffee.weneed.chat.discord.api.handlers.IReactionAddHandler;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 public class HandlerRegistry {
 	private static List<IMessageHandler> messageHandlers = new ArrayList<>();
 	private static List<IJoinHandler> joinHandlers = new ArrayList<>();
+	private static List<IReactionAddHandler> reactionAddHandlers = new ArrayList<>();
 
 	public static void addMessageHandler(IMessageHandler handler){
 		messageHandlers.add(handler);
@@ -18,11 +20,19 @@ public class HandlerRegistry {
 		joinHandlers.add(handler);
 	}
 
+	public static void addReactionAddHandler(IReactionAddHandler handler) {
+		reactionAddHandlers.add(handler);
+	}
+
 	public static List<IMessageHandler> getMessageHandlers() {
 		return messageHandlers;
 	}
 
 	public static List<IJoinHandler> getJoinHandlers() {
 		return joinHandlers;
+	}
+
+	public static List<IReactionAddHandler> getReactionAddHandlers() {
+		return reactionAddHandlers;
 	}
 }
